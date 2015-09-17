4c51a5c7ba06b5308654d1a30dd6019b1e633575
Robert Wilde <robert@wildetech.com.au>
2015-09-17 17:41:55 +1000

    Files layout Update
    change file code layout and updated the doc lines

--- admin/js/gmap-zones-admin.js
+++ admin/js/gmap-zones-admin.js
@@ -1,31 +1,31 @@
-jQuery(document).ready(function($) {
-	/**
-	 * All of the code for your admin-specific JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note that this assume you're going to use jQuery, so it prepares
-	 * the $ function reference to be used within the scope of this
-	 * function.
-	 *
-	 * From here, you're able to define handlers for when the DOM is
-	 * ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * Or when the window is loaded:
-	 *
-	 * $( window ).load(function() {
+jQuery(document).ready(function ($) {
+    /**
+     * All of the code for your admin-specific JavaScript source
+     * should reside in this file.
+     *
+     * Note that this assume you're going to use jQuery, so it prepares
+     * the $ function reference to be used within the scope of this
+     * function.
+     *
+     * From here, you're able to define handlers for when the DOM is
+     * ready:
+     *
+     * $(function() {
 	 *
 	 * });
+     *
+     * Or when the window is loaded:
+     *
+     * $( window ).load(function() {
 	 *
-	 * ...and so on.
-	 *
-	 * Remember that ideally, we should not attach any more than a single DOM-ready or window-load handler
-	 * for any particular page. Though other scripts in WordPress core, other plugins, and other themes may
-	 * be doing this, we should try to minimize doing that in our own work.
-	 */
+	 * });
+     *
+     * ...and so on.
+     *
+     * Remember that ideally, we should not attach any more than a single DOM-ready or window-load handler
+     * for any particular page. Though other scripts in WordPress core, other plugins, and other themes may
+     * be doing this, we should try to minimize doing that in our own work.
+     */
 
     // Code for docs demos
     function createColorpickers() {
@@ -33,7 +33,7 @@ jQuery(document).ready(function($) {
         var bodyStyle = $('body')[0].style;
         $('#demo_apidemo').colorpicker({
             color: bodyStyle.backgroundColor
-        }).on('changeColor', function(ev) {
+        }).on('changeColor', function (ev) {
             bodyStyle.backgroundColor = ev.color.toHex();
         });
 
@@ -46,12 +46,12 @@ jQuery(document).ready(function($) {
         $('.demo-auto').colorpicker();
 
         // Disabled / enabled triggers
-        $(".disable-button").click(function(e) {
+        $(".disable-button").click(function (e) {
             e.preventDefault();
             $("#demo_endis").colorpicker('disable');
         });
 
-        $(".enable-button").click(function(e) {
+        $(".enable-button").click(function (e) {
             e.preventDefault();
             $("#demo_endis").colorpicker('enable');
         });
@@ -60,13 +60,13 @@ jQuery(document).ready(function($) {
     createColorpickers();
 
     // Create / destroy instances
-    $('.demo-destroy').click(function(e) {
+    $('.demo-destroy').click(function (e) {
         e.preventDefault();
         $('.demo').colorpicker('destroy');
         $(".disable-button, .enable-button").off('click');
     });
 
-    $('.demo-create').click(function(e) {
+    $('.demo-create').click(function (e) {
         e.preventDefault();
         createColorpickers();
     });
